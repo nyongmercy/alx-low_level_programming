bd77d63e13fd03082a9dcf5bf4ce065ea4061c9f
nyongmercy <nyongmercy@gmail.com>
2022-07-07 09:01:32 -0700

    Fix the print_remaining_days

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -4,7 +4,6 @@
 * convert_day - converts day of month to day of year, without accounting
 * for leap year
 * @month: month in number format
-* @month: month in number format
 * @day: day of month 
 * Return: day of year
 */
