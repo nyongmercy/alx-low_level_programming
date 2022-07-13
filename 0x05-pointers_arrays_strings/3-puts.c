70b6b9d314b3c563c795a468b9ea0519ac70a12e
nyongmercy <nyongmercy@gmail.com>
2022-07-12 18:15:00 -0700

    ALX C-pointers, arrays and string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,19 @@
+#include"main.h"
+
 /**
-* _puts - Prints a string.
-* @str: string.
+ * _puts - prints a string, followed by a newline to stdout
+ *
+ * @str: string parameter to print
+ *
+ * Return: Nothing
 */
+
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);
-
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		++str;
+	}
 	_putchar('\n');
 }
