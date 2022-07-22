73dd8021603c80e8e9d2779259c9f2c53bbbde0a
nyongmercy <nyongmercy@gmail.com>
2022-07-22 15:41:03 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * Header file containing the prototypes of all functions included in the static library libmy.a
-*/
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -26,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
