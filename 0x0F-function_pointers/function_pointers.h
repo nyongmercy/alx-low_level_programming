6bbd77787b2e0a7764311637b4cb5fe4b1f64793
nyongmercy <nyongmercy@gmail.com>
2022-08-03 17:50:56 -0700

    int int_index(int *array, int size, int (*cmp)(int));

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-int _putchar(char); 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
