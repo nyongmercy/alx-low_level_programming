da35c6ca77267eb5143e03d2dff46279e84c37ce
nyongmercy <nyongmercy@gmail.com>
2022-08-15 16:58:43 -0700

    hot fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,24 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t node_count = 1;
 
-	while (h)
+	/* return 0 as no of nodes when h is null*/
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
 	{
-		nodes++;
 		printf("%d\n", h->n);
+
+		/*go to the next node*/
 		h = h->next;
+		/*count no of nodes*/
+		node_count++;
 	}
 
-	return (nodes);
+	/*print last node*/
+	printf("%d\n", h->n);
+
+	return (node_count);
 }
